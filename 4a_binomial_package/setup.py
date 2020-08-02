1d73c0f6730ad3730bd43dc13c39ed776359c24b
boluwatifeh <tifeasypeasy@gmail.com>
2020-08-02 04:04:38 +0000

    completed exercise four

---
+++ 4a_binomial_package/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='dsnd-probability',
+      
+      version='0.1',
+      
+      description='Gaussian and Binomial distributions',
+      
+      packages=['dsnd-probability'],
+      
+      author = 'Ogundeyi Boluwatife',
+     
+     author_email = 'tifeasypeasy@gmail.com',
+      
+  zip_safe=False)
